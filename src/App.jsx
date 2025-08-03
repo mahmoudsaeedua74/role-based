86149fae4d870b1ad0030626959764bb634c15dc
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-08-04 00:42:41 +0300

    feat: add Toaster for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Register from "./pages/auth/Register";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const router = createBrowserRouter([
     // for login users
     {
       path: "/login",
-      element: <Login />,
+      element: <Register />,
     },
   ]);
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Toaster />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
