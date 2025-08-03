81bd0a1d25c78be430480e298981e4e13dd7898e
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-08-04 00:25:23 +0300

    feat: add login form  and dynamic input

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,23 @@
 import { useForm } from "react-hook-form";
-
+// login form data
+const loginFormData = [
+  {
+    label: "Email address",
+    name: "email",
+    placeholder: "Put your email",
+    type: "email",
+  },
+  {
+    label: "Password",
+    name: "password",
+    placeholder: "Put your password",
+    type: "password",
+  },
+];
 export default function Register() {
   const {
+    register,
+    formState: { errors, isValid },
     handleSubmit,
   } = useForm({
     mode: "onTouched",
@@ -21,6 +37,23 @@ export default function Register() {
           className="flex flex-col gap-4"
           noValidate
         >
+          {loginFormData.map((formInput, idx) => (
+            <div key={idx} className="flex flex-col space-y-2">
+              <label
+                htmlFor={formInput.name}
+                className="text-gray-500 font-medium"
+              >
+                {formInput.label}
+              </label>
+              <Input
+                name={formInput.name}
+                type={formInput.type}
+                placeholder={formInput.placeholder}
+                register={register}
+                errors={errors}
+              />
+            </div>
+          ))}
           <Link
             to="/forgot-password"
             className="text-sm text-gray-700 text-end hover:underline"
@@ -29,8 +62,13 @@ export default function Register() {
           </Link>
           <button
             type="submit"
+            disabled={!isValid}
             className={`w-full flex items-center justify-center gap-2 py-2 rounded-md font-medium transition duration-200 border border-primary
-            bg-primary text-white hover:bg-white hover:text-black
+            ${
+              !isValid
+                ? "opacity-50 cursor-not-allowed bg-gray-300 text-gray-500 border-gray-300"
+                : "bg-primary text-white hover:bg-white hover:text-black"
+            }
             `}
           >
             Login
