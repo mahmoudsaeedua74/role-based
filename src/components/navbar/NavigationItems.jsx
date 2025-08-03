b83ddba05e8db8ca3a67ec1cec94ea42bca8b6c1
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-08-04 02:34:29 +0300

    refactor  :  rename navbar to Sidebar component with responsive and add a new components  for NavigationItems

--- src/components/navbar/NavigationItems.jsx
+++ src/components/navbar/NavigationItems.jsx
@@ -1,55 +1,45 @@
-import { useDispatch, useSelector } from "react-redux";
-import logo from "/logo.webp";
-import { logout } from "../../store/sliceAuth/authSlice";
-import { sidebarItems } from "../../data";
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
+import { sidebarItems } from "../../data";
+import { defaultMenuItemVariants } from "../../data/motionVariants";
 
-export default function Navbar() {
-  const dispatch = useDispatch();
-
-  const handleLogout = () => {
-    localStorage.clear();
-    dispatch(logout());
-  };
+const NavigationItems = ({ handleNavLinkClick, handleLogout }) => {
   const permissions =
     useSelector((state) => state.auth.user?.permissions) ||
     JSON.parse(localStorage.getItem("user"))?.permissions;
-
   const filteredItems = sidebarItems.filter((item) =>
     permissions?.includes(item.permission)
   );
   return (
-    <nav className="bg-sidebar text-[#9E9E9E] font-semibold py-10 px-4 fixed top-0 bottom-0 w-[300px] items-center flex-col gap-10 z-50 hidden md:flex">
-      <img
-        src={logo}
-        alt="Logo"
-        className={`size-28 object-cover rounded-full `}
-      />
-
+    <>
       <div className="gap-4 flex flex-col w-full">
         {filteredItems.map((item) => (
-          <div key={item.path}>
+          <motion.div key={item.path} variants={defaultMenuItemVariants}>
             <NavLink
               to={item.path}
-              className="p-4 hover:rounded-lg hover:bg-primary hover:scale-105 hover:text-white duration-200 transition-all block"
+              className="p-4 sidebar block"
+              onClick={handleNavLinkClick}
             >
               <div className="flex items-center gap-4">
                 <item.icon />
                 <span>{item.title}</span>
               </div>
             </NavLink>
-          </div>
+          </motion.div>
         ))}
       </div>
-      <div className="w-full mt-20 ">
+      <motion.div variants={defaultMenuItemVariants} className="w-full mt-20 ">
         <button
           type="button"
-          onClick={handleLogout}
+          onClick={() => handleLogout()}
           className="w-full flex items-center self-end justify-center gap-2 py-3 px-4 rounded-md font-medium transition-all duration-200 border border-[#F77737] bg-[#F77737] text-white hover:bg-white hover:text-black"
         >
           <span className="text-sm font-semibold">Logout</span>
         </button>
-      </div>
-    </nav>
+      </motion.div>
+    </>
   );
-}
+};
+
+export default NavigationItems;
---
+++ src/components/navbar/Sidebar.jsx
@@ -0,0 +1,144 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/sliceAuth/authSlice";
+import { IoMdMenu } from "react-icons/io";
+import { MdClose } from "react-icons/md";
+import { AnimatePresence, motion } from "framer-motion";
+
+import Logo from "../ui/Logo";
+import NavigationItems from "./NavigationItems";
+import {
+  containerVariant,
+  menuItemVariants,
+  overlayVariants,
+  sidebarVariants,
+} from "../../data/motionVariants";
+import { useNavigate } from "react-router-dom";
+
+export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    localStorage.clear();
+    dispatch(logout());
+    setIsOpen(false);
+    navigate("/login");
+  };
+
+  //to close the mobile sidebar when window size becomes desktop
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  // to stope user from scroll if he opens the sidebar
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "auto";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
+  return (
+    <>
+      {/* Desktop*/}
+      <AnimatePresence>
+        <motion.nav
+          variants={sidebarVariants}
+          initial="closed"
+          animate="open"
+          exit="closed"
+          className="bg-sidebar text-[#9E9E9E] font-semibold py-10 px-4 fixed top-0 bottom-0 w-[300px] items-center flex-col gap-10 z-50 hidden md:flex"
+        >
+          <motion.div variants={menuItemVariants}>
+            <Logo />
+          </motion.div>
+          <NavigationItems
+            handleNavLinkClick={() => setIsOpen(false)}
+            handleLogout={handleLogout}
+          />
+        </motion.nav>
+      </AnimatePresence>
+
+      {/* Mobile Header */}
+      <div className="flex justify-between p-3 items-center  md:hidden border-b">
+        <Logo size={"size-20"} />
+        <button
+          onClick={() => setIsOpen(true)}
+          className="size-12 cursor-pointer hover:bg-slate-200 rounded-full flex items-center justify-center transition-colors duration-200"
+          aria-label="Toggle menu"
+        >
+          <IoMdMenu size={30} />
+        </button>
+      </div>
+      {/* Mobile Sidebar */}
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            {/* Overlay */}
+            <motion.div
+              className="fixed inset-0 bg-black bg-opacity-50 z-40 md:hidden"
+              variants={overlayVariants}
+              initial="closed"
+              animate="open"
+              exit="closed"
+              onClick={() => setIsOpen(false)}
+            />
+
+            {/* Mobile Sidebar */}
+            <motion.nav
+              className="bg-[#2C2C2C] text-[#9E9E9E] font-semibold py-6 px-4 fixed top-0 bottom-0 w-[300px] z-50 md:hidden flex flex-col gap-6"
+              variants={sidebarVariants}
+              initial="closed"
+              animate="open"
+              exit="closed"
+            >
+              <motion.div
+                variants={containerVariant}
+                initial="closed"
+                animate="open"
+                exit="closed"
+                className="flex flex-col gap-6 flex-1 "
+              >
+                {/* Close Button */}
+                <motion.div
+                  variants={menuItemVariants}
+                  className="w-fit ms-auto"
+                >
+                  <motion.button
+                    onClick={() => setIsOpen(false)}
+                    className="p-2 size-12 hover:text-black cursor-pointer group hover:bg-slate-200 rounded-full transition-all duration-200"
+                    aria-label="Close menu"
+                  >
+                    <MdClose
+                      size={30}
+                      className="text-white group-hover:text-black group-hover:rotate-180 transition-all duration-200"
+                    />
+                  </motion.button>
+                </motion.div>
+
+                {/* Logo */}
+                <motion.div variants={menuItemVariants} className="self-center">
+                  <Logo />
+                </motion.div>
+
+                {/* Navigation Items */}
+                <motion.div variants={menuItemVariants}>
+                  <NavigationItems
+                    handleNavLinkClick={() => setIsOpen(false)}
+                    handleLogout={handleLogout}
+                  />
+                </motion.div>
+              </motion.div>
+            </motion.nav>
+          </>
+        )}
+      </AnimatePresence>
+    </>
+  );
+}
