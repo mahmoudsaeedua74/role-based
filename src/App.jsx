b536c67fd8e4143650245c5bbf55c76b70fe4140
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-08-03 23:56:54 +0300

    feat: setup the app routes and add /login as the first route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+
 function App() {
-  return (
-    <>
-    </>
-  );
+  const router = createBrowserRouter([
+    // for login users
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
