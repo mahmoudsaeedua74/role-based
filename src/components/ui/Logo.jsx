068732a41d32a96a5550c29fbf0e17dd72373798
Mahmoud saeed <Mahmoudsaeed0112074@gamil.com>
2025-08-04 11:45:59 +0300

    feat: enhance Logo component to display user avatar and name

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -1,11 +1,24 @@
+import { useSelector } from "react-redux";
 import logo from "/logo.webp";
 
-const Logo = ({ size = "size-28", className = "" }) => (
-  <img
-    src={logo}
-    alt="Logo of page"
-    className={`${size} object-cover rounded-full ${className}`}
-  />
-);
+const Logo = ({ size = "size-28", className = "" }) => {
+  const user =
+    useSelector((state) => state.auth.user) ||
+    JSON.parse(localStorage.getItem("user"));
+
+  const imageSrc = user?.avatar || logo;
+  const userName = user?.name || "App Name";
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <img
+        src={imageSrc}
+        alt="Logo or User Avatar"
+        className={`${size} object-cover rounded-full ${className}`}
+      />
+      <span className="text-lg font-semibold text-primary">{userName}</span>
+    </div>
+  );
+};
 
 export default Logo;
